a4601c1cb764b2f19fa0c6ad230d67fb628dbb3b
Nikita <logachovnik@gmail.com>
2021-02-14 11:18:53 +0200

    Добавьте файлы проекта.

---
+++ Pointers_Pr11/Pointers_Pr11.cpp
@@ -0,0 +1,40 @@
+﻿/*
+	Указники. Приклад 11
+Створити указники на указники. 
+Розібрати пріоритети операцій * і ++
+*/
+
+#include <iostream>
+#include <Windows.h>
+#define N 5
+using namespace std;
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);	
+	int A[N] = { 0,1,2,3,4 };
+	int* p[N] = { A, A + 1, A + 2, A + 3, A + 4 };
+	int** pp = p;
+	for (int i = 0; i < N; i++)
+	{
+		cout << "p[" << i << "] = " << p[i];
+		cout << " : *p[" << i << "] = ";
+		cout << *p[i] << endl;
+	}
+	cout << "*p[0]++ = " << *p[0]++ << endl;
+	cout << "p[0] = " << p[0] << " : *p[0] = ";
+	cout << *p[0] << endl;
+	cout << "*++p[0] = " << *++p[0] << endl;
+	cout << "p[0] = " << p[0] << " : *p[0] = ";
+	cout << *p[0] << endl;
+
+	// Повертаємо в початковий стан масив указників
+	for (int i = 0; i < N; i++)
+	{
+		p[i] = &A[i];
+		cout << "p[" << i << "] = " << p[i];
+		cout << " : *p[" << i << "] = ";
+		cout << *p[i] << endl;
+	}
+}
---
+++ Pointers_Pr7.2/Pointers_Pr7.2.cpp
@@ -0,0 +1,70 @@
+﻿/*
+	Указники. Приклад 7.2
+В одновимірному динамічному масиві поміняти місцями
+останній і мінімальний елеманти
+*/
+
+#include <iostream>
+#include <time.h>       /* time */
+#include <Windows.h>
+using namespace std;
+
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	srand(time(NULL));
+	// Встановлення розміру масиву
+	int SIZE = 5;
+
+	// Резервування місця в пам'яті під масив
+	// з int елементів в кількості SIZE
+	int* pArr = new int[SIZE];
+	// Запам'ятовування початку масиву
+	int* pArr_start = pArr;
+	// Указник на мінімальний елемент
+	int* pMin;
+
+	// Заповнення масиву
+	cout << "Початковий масив: \n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		*pArr = rand() % 10 - 5;
+		cout << i << " " << pArr << " ";
+		cout << *pArr << endl;
+		pArr++;
+	}
+
+	// Тимчасова змінна для обміну
+	int temp, i, iMin;
+	// Пошук мінімального елемента
+	for (i = 0, iMin = 0, pMin = pArr_start, 
+		pArr = pArr_start; i < SIZE; i++)
+	{
+		if (*pArr < *pMin) { pMin = pArr; iMin = i; }
+		pArr++;
+	}
+	cout << "Мінімальний елемент:\n";
+	cout << iMin << " " << pMin << " ";
+	cout << *pMin << endl;
+	// Повернення указника на початок масиву знову
+	pArr = pArr_start;
+	// Обмін
+	temp = *pMin;
+	*pMin = *(pArr + SIZE - 1);
+	*(pArr + SIZE - 1) = temp;
+	
+	// Друк результату
+	cout << "Результуючий масив: \n";
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << i << " " << pArr << " ";
+		cout << *pArr << endl;
+		pArr++;
+	}
+
+	// Повернення указника на початок масиву знову
+	pArr = pArr_start;
+	// Звільнення пам'яті
+	delete[] pArr;
+}
